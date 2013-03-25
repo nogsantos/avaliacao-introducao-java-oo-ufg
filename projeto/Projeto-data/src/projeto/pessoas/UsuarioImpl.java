97c314bbb3e57f29d7a01c12f521081ad28d646f
fabricionogueira <fabricionogueira@localhost>
2013-03-25 16:46:14 -0300

    Otimização do código e implementação do crud e listagem dos módulos.

---
+++ projeto/Projeto-data/src/projeto/pessoas/UsuarioImpl.java
@@ -0,0 +1,81 @@
+/**
+ *
+ * Descrição:Classe UsuarioImpl
+ *
+ *
+ * @author Fabricio Nogueira
+ *
+ * @since 25-Mar-2013
+ *
+ * @version 1.0.0
+ *
+ */
+
+package projeto.pessoas;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class UsuarioImpl implements UsuarioDAO{
+
+    @Override
+    public boolean cadastrar(Usuario usuario) throws SQLException {
+        /**
+         * Método cadastrar
+         *
+         * @author Fabricio Nogueira
+         * @version 1.0.0
+         *
+         */
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean editar(Usuario usuario) throws SQLException {
+        /**
+         * Método editar
+         *
+         * @author Fabricio Nogueira
+         * @version 1.0.0
+         *
+         */
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean excluir(Usuario usuario) throws SQLException {
+        /**
+         * Método excluir
+         *
+         * @author Fabricio Nogueira
+         * @version 1.0.0
+         *
+         */
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Usuario> listar() throws SQLException {
+        /**
+         * Método listar
+         *
+         * @author Fabricio Nogueira
+         * @version 1.0.0
+         *
+         */
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Usuario getByCodigo(int codigo) throws SQLException {
+        /**
+         * Método getByCodigo
+         *
+         * @author Fabricio Nogueira
+         * @version 1.0.0
+         *
+         */
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
